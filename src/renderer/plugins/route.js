eb08d06f04cc3aa07b4927fc4949b967ba5f6df2
aristidelegba <aristidelegba67@gmail.com>
2022-03-21 15:43:38 +0100

    wip : setting up the files branch

--- src/renderer/plugins/route.js
+++
@@ -1,7 +0,0 @@
-export default ({ app, store }) => {
-  app.router.beforeEach((to, from, next) => {
-    store.dispatch("initializeTimeframes", {});
-    next();
-  });
-  app.router.afterEach((to, from) => {});
-};
